1b440ff570f4d9895a8ee25682203688f9b28775
andcia <andreciappara@gmail.com>
2017-08-16 13:09:34 +0200

    Hat moving out of screen
    Although the hat is moving well according to the commands given by the
    user, the hat is going of screen therefore we need to settle the world
    space for the hat and assign boundaries.

--- Catch_Game/Assets/Scripts/Hat_controller.cs
+++ Catch_Game/Assets/Scripts/Hat_controller.cs
@@ -5,21 +5,30 @@ using UnityEngine;
 public class Hat_controller : MonoBehaviour {
 
 	public Camera cam;
+
+	private float maxWidth;
+
 	// Use this for initialization
 	void Start () {
 
 		if (cam == null)
 		{
-			cam = Camera.main;
+			cam = Camera.main;//using the same camera towards screen world.
 		}
+
+		Vector3 upperCorner = new Vector3(Screen.width, Screen.height, 0.0f);//knowing the position of the screen upper corner
+		Vector3 targetWidth = cam.ScreenToWorldPoint(upperCorner);
+		maxWidth = targetWidth.x;
 	}
 	
 	// Update is called once physics timestep
 	void FixedUpdate () {
 
-		Vector3 rawPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+		Vector3 rawPosition = cam.ScreenToWorldPoint(Input.mousePosition);// follows the path of the mouse right and left on the screen
 		Vector3 targetPosition = new Vector3(rawPosition.x, 0.0f, 0.0f);
-		GetComponent<Rigidbody2D>().MovePosition(targetPosition);
+		float targetWidth = Mathf.Clamp(targetPosition.x, -maxWidth, maxWidth);//Everything is centered on the center screen on the scene therefore our target position is able to set.
+		targetPosition = new Vector3(targetWidth, targetPosition.y, targetPosition.x);//clamping the target postion between x and y
+		GetComponent<Rigidbody2D>().MovePosition(targetPosition);// the movement (mouse)
 		
 	}
 }
