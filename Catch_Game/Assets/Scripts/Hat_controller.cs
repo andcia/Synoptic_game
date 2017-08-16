7bffc1ec76851827296e54415fae928dded61d17
andcia <andreciappara@gmail.com>
2017-08-16 12:25:55 +0200

    Using the Camera script and fixed update instead of update
    The camera variable cam is set to public that means it’s accessible
    from all scripts in the game. Also the fixed update is now called once
    per physics timestep rather than once per frame which can be used in
    the physics settings.
    
    The ScreenToWorldPoint will work on both mouse and smartphones or on
    most smartphones as the code works directly on the software itself.

--- Catch_Game/Assets/Scripts/Hat_controller.cs
+++ Catch_Game/Assets/Scripts/Hat_controller.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class Hat_controller : MonoBehaviour {
 
+	public Camera cam;
 	// Use this for initialization
 	void Start () {
-		
+
+		if (cam == null)
+		{
+			cam = Camera.main;
+		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	// Update is called once physics timestep
+	void FixedUpdate () {
+
+		Vector3 rawPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+		Vector3 targetPosition = new Vector3(rawPosition.x, 0.0f, 0.0f);
+		GetComponent<Rigidbody2D>().MovePosition(targetPosition);
 		
 	}
 }
