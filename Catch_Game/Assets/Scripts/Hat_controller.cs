15e9e3ba41f0c14f1ce2134202478c7a1eaae71a
andcia <andreciappara@gmail.com>
2017-08-16 13:15:04 +0200

    Hat positioning on screen
    Hat is set to a boundary against the far end of the screen, therefore
    we assign a renderer to prove the size of the hat according to the
    movement of the mouse. This will make the hat hit the boundary and
    stop, which will allow the user to see the full hat displayed from the
    edge of the object not from the centre position assigned in the
    beginning of the scene input.

--- Catch_Game/Assets/Scripts/Hat_controller.cs
+++ Catch_Game/Assets/Scripts/Hat_controller.cs
@@ -8,6 +8,8 @@ public class Hat_controller : MonoBehaviour {
 
 	private float maxWidth;
 
+	public Renderer rend;
+
 	// Use this for initialization
 	void Start () {
 
@@ -15,10 +17,12 @@ public class Hat_controller : MonoBehaviour {
 		{
 			cam = Camera.main;//using the same camera towards screen world.
 		}
+		rend = GetComponent<Renderer>();
 
 		Vector3 upperCorner = new Vector3(Screen.width, Screen.height, 0.0f);//knowing the position of the screen upper corner
 		Vector3 targetWidth = cam.ScreenToWorldPoint(upperCorner);
-		maxWidth = targetWidth.x;
+		float hatWidth = rend.bounds.extents.x;
+		maxWidth = targetWidth.x - hatWidth;
 	}
 	
 	// Update is called once physics timestep
