831e29389f97e179763318691e36bb7572a498e8
andcia <andreciappara@gmail.com>
2017-08-22 15:16:24 +0200

    Adding, Score Script

--- Catch_Game/Assets/Scripts/GameController.cs
+++ Catch_Game/Assets/Scripts/GameController.cs
@@ -1,51 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
 	public Camera cam;
 	public GameObject[] ball;
-	private Renderer rend;
-
 	public float timeLeft;
+	public Text timerText;
+	public GameObject gameOverText;
+	public GameObject restartButton;
+	public GameObject splashScreen;
+	public GameObject startButton;
+	public Hat_controller hatController;
 
 	private float maxWidth;
+	private bool counting;
 
 	// Use this for initialization
 	void Start()
 	{
-
 		if (cam == null)
 		{
-			cam = Camera.main;//using the same camera towards screen world.
+			cam = Camera.main;
 		}
-		rend = GetComponent<Renderer>();
-
-		Vector3 upperCorner = new Vector3(Screen.width, Screen.height, 0.0f);//knowing the position of the screen upper corner
-		Vector3 targetWidth = cam.ScreenToWorldPoint(upperCorner);//Locating the whole screen and with the ScreenToWorld value upperCorner we can determine the outcome position of the camera
+		Vector3 upperCorner = new Vector3(Screen.width, Screen.height, 0.0f);
+		Vector3 targetWidth = cam.ScreenToWorldPoint(upperCorner);
 		float ballWidth = ball[0].GetComponent<Renderer>().bounds.extents.x;
+		timerText.text = "Time:\n" + Mathf.RoundToInt(timeLeft);
 		maxWidth = targetWidth.x - ballWidth;
-		StartCoroutine(Spawn());
 	}
 
-	void FixedUpdate() {
-		timeLeft -= Time.deltaTime;
-	}
-	IEnumerator Spawn()
+	void FixedUpdate()
 	{
-		yield return new WaitForSeconds(2.0f);//giving the player some setup time frame to adjust the controls before the game commences with the balls
+		if (counting)
+		{
+			timeLeft -= Time.deltaTime;
+			if (timeLeft < 0)
+			{
+				timeLeft = 0;
+			}
+			timerText.text = "TIME LEFT:\n" + Mathf.RoundToInt(timeLeft);
+		}
+	}
+
+	public void StartGame()
+	{
+		splashScreen.SetActive(false);
+		startButton.SetActive(false);
+		StartCoroutine(Spawn());
+		timerText.text = "TIME LEFT:\n" + Mathf.RoundToInt(timeLeft);
+	}
+
+	public IEnumerator Spawn()
+	{
+		yield return new WaitForSeconds(2.0f);
+		counting = true;
 		while (timeLeft > 0)
 		{
+			GameObject balls = ball[Random.Range(0, ball.Length)];
 			Vector3 spawnPosition = new Vector3(
-				Random.Range(-maxWidth, maxWidth),
+				transform.position.x + Random.Range(-maxWidth, maxWidth),
 				transform.position.y,
-				0.0f);//Gameobjects location X,Y,Z
+				0.0f
+			);
 			Quaternion spawnRotation = Quaternion.identity;
-			Instantiate<GameObject>(ball[0], spawnPosition, spawnRotation);
-
-			//Asking unity to stop the loop and return new random range
+			Instantiate(balls, spawnPosition, spawnRotation);
 			yield return new WaitForSeconds(Random.Range(1.0f, 2.0f));
 		}
+		yield return new WaitForSeconds(2.0f);
+		gameOverText.SetActive(true);
+		yield return new WaitForSeconds(2.0f);
+		restartButton.SetActive(true);
 	}
 }
