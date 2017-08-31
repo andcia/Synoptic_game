d4abc883787405da128082fe299fb51524a3a062
andcia <andreciappara@gmail.com>
2017-08-31 23:49:37 +0200

    Scripts, Start & Restart
    Right after the bowling ball is being able to hit the collider
    (HatBackSprite), the next thing was to get the bombs up and running in
    a random position and also during a random time. Once the bomb ball is
    dropped, the the DestroyOnContact script is active and calls for the
    Explosion from the Materials set from the prefab.
    
    Points & Timer
    Points are controlled in the game controller that is specifically
    reading the collider from the hat whilst picking the next object
    falling. If the bowling ball is falling hitting the hat’s collider.
    Once the object hat reaches the collider, a point is added to the
    score, if the player fails to pick the bowling ball no points are give.
    On the other hand when the bombs hit the collider 2 points are deducted
    from the score.
    
    IENumerator has been used to respawn the bowling balls and also the
    bombs whilst having the ScreenToWorldPoint that determined the area
    space a bowling ball and the bomb will fall, measuring the whole screen
    for the to be able to collect the bowling ball. This is the same thing
    when the hat was set to be displayed at all time given edge (Screen
    limit). The timer has been set by me at 10 seconds, set in the
    GameController ‘TimeLeft’. (In Future i would add a game option for the
    user to choose between different timers.)
    
    Start, Splash and Restart
    
    The start button is is assigned to the HT_StartGame script, and calls
    for an ‘onmousedown’ action by the user which will load the
    GameController and starts the game scene. Also the timer once the 10
    seconds are up, it will display a Game Over label, and 2 seconds after
    displays the restart button. The reset button will recall the scene
    from the start, resetting the score and timer.

---
+++ Catch_Game/Assets/Scripts/Scripts_Main_Menu/StartOptions.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+
+public class StartOptions : MonoBehaviour {
+
+
+
+	public int sceneToStart = 1;										//Index number in build settings of scene to load if changeScenes is true
+	public bool changeScenes;											//If true, load a new scene when Start is pressed, if false, fade out UI and continue in single scene									//Choose whether to continue playing menu music or start a new music clip
+
+
+	[HideInInspector] public bool inMainMenu = true;					//If true, pause button disabled in main menu (Cancel in input manager, default escape key)									//Very short fade time (10 milliseconds) to start playing music immediately without a click/glitch
+	private ShowPanels showPanels;										//Reference to ShowPanels script on UI GameObject, to show and hide panels
+
+	
+	void Awake()
+	{
+		//Get a reference to ShowPanels attached to UI object
+		showPanels = GetComponent<ShowPanels> ();
+	}
+
+
+	public void StartButtonClicked()
+	{
+			//Call the StartGameInScene function to start game without loading a new scene.
+			StartGameInScene();
+	}
+
+
+	public void LoadDelayed()
+	{
+		//Pause button now works if escape is pressed since we are no longer in Main menu.
+		inMainMenu = false;
+
+		//Hide the main menu UI element
+		showPanels.HideMenu ();
+
+		//Load the selected scene, by scene index number in build settings
+		SceneManager.LoadScene (sceneToStart);
+	}
+
+	public void HideDelayed()
+	{
+		//Hide the main menu UI element after fading out menu for start game in scene
+		showPanels.HideMenu();
+	}
+
+	public void StartGameInScene()
+	{
+		//Pause button now works if escape is pressed since we are no longer in Main menu.
+		inMainMenu = false;
+		SceneManager.LoadScene("Main");
+		Debug.Log ("Game started in same scene! Put your game starting stuff here.");
+	}
+}
---
+++ Catch_Game/Assets/Scripts/Start_Game.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start_Game : MonoBehaviour {
+
+	public GameController gameController;
+
+	void OnMouseDown()
+	{
+		gameController.StartGame();
+	}
+}
